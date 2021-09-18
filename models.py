7a45a27e1f635629d01eaea59a609d0786574ad2
Haley Rairez <ramirezhaley@gmail.com>
2021-09-18 16:24:05 -0500

    i think i figured out embedding

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from torch import optim
 import numpy as np
 import random
 from sentiment_data import *
+from torch.utils.data import Dataset, DataLoader
 
 
 class SentimentClassifier(object):
@@ -46,21 +47,21 @@ class NeuralSentimentClassifier(SentimentClassifier):
     Implement your NeuralSentimentClassifier here. This should wrap an instance of the network with learned weights
     along with everything needed to run it on new data (word embeddings, etc.)
     """
-    def __init__(self):
-        self.network = DANClassifier(1,32,2)
+    def __init__(self, model):
+        self.network = model
 
 class DANClassifier(torch.nn.Module):
-    def __init__(self, inp=1, hid=32, out=2):
+    def __init__(self, embeddings, inp=1, hid=32, out=2):
         super(DANClassifier, self).__init__()
-        self.embedding = torch.nn.Embedding(5000, 40, padding_idx=0)
+        self.embedding = nn.Embedding.from_pretrained(torch.from_numpy(embeddings.vectors), padding_idx=0)
         self.V = nn.Linear(inp, hid)
         # self.g = nn.Tanh()
         self.g = nn.ReLU()
         self.W = nn.Linear(hid, out)
         self.log_softmax = nn.LogSoftmax(dim=0)
         # Initialize weights according to a formula due to Xavier Glorot.
-        nn.init.xavier_uniform_(self.V.weight)
-        nn.init.xavier_uniform_(self.W.weight)
+        # nn.init.xavier_uniform_(self.V.weight)
+        # nn.init.xavier_uniform_(self.W.weight)
 
     def forward(self, x):
         #take embedding
@@ -84,54 +85,40 @@ def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_ex
     batch_size = 32
 
     # Model specifications
-    model = DANClassifier()
+    model = DANClassifier(word_embeddings)
     optimizer = optim.Adam(model.parameters(), lr=initial_learning_rate)
     loss = torch.nn.NLLLoss() # because of soft max
 
+    train_data =  load_data(train_exs, word_embeddings)
+    dev_data = load_data(dev_exs, word_embeddings)
+
 
     for epoch in range(num_epochs):
-        #shuffle data
-        ex_indices = [i for i in range(0, len(train_exs))]
-        random.shuffle(ex_indices)
-
-        # set sepoch level varibles
+        # set epoch level varibles
         total_loss = 0.0
         accuracys = []
 
-        for idx in ex_indices:
-            #create batch
-
-            #### batch here 
-            # for each sample  paddinx* total lengths
-            
-            ### grab first n-batch size  put into matrix/n dim tensor [batchsize, len_sent (52?)] [32 x 32 x len vocab - how to get?]
-            ### pad with np 0 array
-            #####
-            x = train_exs[idx].words
-            y_true = train_exs[idx].label
-
-            # iterrate 
-            # use indeter to convert from word to indices - pass to embedder 
-            #  word_embeddings.word_indexer
-            # nn.em
-            # self.embedding = torch.nn.Embedding.from_pretrained - look up on pytorch(from indexer, 52, padding_idx=0, from_pretrained - glove 300-d)
-            # do embedding sperately for each item in the batch
-            # run on each word and each example - nested for loop
-
+        for data, labels in train_data:
+            print(len(data))
+            print(len(labels))
             # run through model
             model.zero_grad()
-            y_pred = model.forward(x)
+            y_pred = model.forward(data)
             
             # calculate loss and accuracy
-            total_loss += loss(y_pred, y_true)
-            accuracys.append(calculate_accuracy(y_pred, y_true))
+            total_loss += loss(y_pred, labels)
+            accuracys.append(calculate_accuracy(y_pred, labels))
             
             # Computes the gradient and takes the optimizer step
             loss.backward()
             optimizer.step()
 
+        #work trough dev examples
+
         print("Total loss on epoch %i: %f" % (epoch, total_loss))
         print("The accuracy for epoch %i: %f" % (epoch, np.mean(accuracys)))
+
+    final = NeuralSentimentClassifier(model)
 
 
 def calculate_accuracy(y_predict, y_true):
@@ -141,6 +128,38 @@ def calculate_accuracy(y_predict, y_true):
         state = 1 if y_predict[index] == y_true[index] else 0
         acc.append(state)
     return np.mean(acc)
+
+
+class Words(Dataset):
+    def __init__(self, examples, word_embeddings: WordEmbeddings):
+        self.count = 0
+        self.data = []
+
+        for item in examples:
+            
+            #pass to indexer
+            new_sentence =  []
+            for word in item.words:
+                index = word_embeddings.word_indexer.add_and_get_index(word, add=False) if word_embeddings.word_indexer.add_and_get_index(word, add=False) != -1 else 0
+                new_sentence.append(index)
+            
+            # pad with 0
+            new_sentence += [0] * (52 - len(new_sentence))
+            print(len(new_sentence))
+            
+            self.data.append([new_sentence, item.label])
+            self.count += 1
+
+    def __len__(self):
+        return self.count
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+
+def load_data(examples, embedder, num_workers=0, batch_size=32):
+    dataset = Words(examples, embedder)
+    return DataLoader(dataset, num_workers=num_workers, batch_size=batch_size, shuffle=True, drop_last=False)
         
 
 
