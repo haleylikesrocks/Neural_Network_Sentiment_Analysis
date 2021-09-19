8744435aa2a0cc436287d2fffe207eb0086fcb0a
Haley Rairez <ramirezhaley@gmail.com>
2021-09-19 18:27:42 -0500

    batched correctly i think

--- models.py
+++ models.py
@@ -5,6 +5,8 @@ import torch.nn as nn
 from torch import optim
 import numpy as np
 import random
+
+from torch.utils import data
 from sentiment_data import *
 from torch.utils.data import Dataset, DataLoader
 
@@ -22,7 +24,7 @@ class SentimentClassifier(object):
         """
         raise Exception("Don't call me, call my subclasses")
 
-    def predict_all(self, all_ex_words: List[List[str]]) -> List[int]:
+    def predict_all(self, all_ex_words: List[List[str]], not_preprocessed=False) -> List[int]:
         """
         You can leave this method with its default implementation, or you can override it to a batched version of
         prediction if you'd like. Since testing only happens once, this is less critical to optimize than training
@@ -30,7 +32,7 @@ class SentimentClassifier(object):
         :param all_ex_words: A list of all exs to do prediction on
         :return:
         """
-        return [self.predict(ex_words) for ex_words in all_ex_words]
+        return [self.predict(ex_words, not_preprocessed) for ex_words in all_ex_words]
 
 
 class TrivialSentimentClassifier(SentimentClassifier):
@@ -53,28 +55,70 @@ class NeuralSentimentClassifier(SentimentClassifier):
 class DANClassifier(torch.nn.Module):
     def __init__(self, embeddings, inp=1, hid=32, out=2):
         super(DANClassifier, self).__init__()
+        self.word_embeddings = embeddings
         self.embedding = nn.Embedding.from_pretrained(torch.from_numpy(embeddings.vectors), padding_idx=0)
+
         self.V = nn.Linear(inp, hid)
-        # self.g = nn.Tanh()
         self.g = nn.ReLU()
         self.W = nn.Linear(hid, out)
         self.log_softmax = nn.LogSoftmax(dim=0)
-        # Initialize weights according to a formula due to Xavier Glorot.
-        # nn.init.xavier_uniform_(self.V.weight)
-        # nn.init.xavier_uniform_(self.W.weight)
+
+    def preprocess(self, sentence):
+        new_sentence =  []
+        #index sentence
+        for word in sentence:
+            index = self.word_embeddings.word_indexer.index_of(word) if self.word_embeddings.word_indexer.index_of(word) != -1 else 0
+            new_sentence.append(index)
+        #pad with 0
+        new_sentence += [0] * (52 - len(new_sentence))
+
+        #translate to embedding
+        embeddings = []
+        for idx in new_sentence:
+            embeddings.append(self.embedding(torch.tensor(idx)).numpy())
+        ave_embedding = torch.mean(torch.tensor(embeddings), 0)
+
+        return ave_embedding
+            
+    def predict(self, sentence, not_preprocessed=True):
+        x = self.preprocess(sentence) if not_preprocessed else sentence
+        return self.forward(x)
+    
+    def predict_all(self, all_ex_words: List[List[str]], not_preprocessed=True) -> List[int]:
+        """
+        You can leave this method with its default implementation, or you can override it to a batched version of
+        prediction if you'd like. Since testing only happens once, this is less critical to optimize than training
+        for the purposes of this assignment.
+        :param all_ex_words: A list of all exs to do prediction on
+        :return:
+        """
+        return [self.predict(ex_words, not_preprocessed) for ex_words in all_ex_words]
 
     def forward(self, x):
-        #take embedding
-        #average embedding
-        embedded_data = torch.empty((1,300))
-        for i, sentence in enumerate(x):
-            embbedings = torch.empty((1,300))
-            for index, word in enumerate(sentence):
-                embedded_word =  self.embedding(word)
-                embbedings = torch.cat((embbedings, torch.unsqueeze(embedded_word,0)))
-            ave_embed = torch.mean(torch.tensor(embbedings), 0)
-            embedded_data = torch.cat((embedded_data, torch.unsqueeze(ave_embed, 0)))  
-        return self.log_softmax(self.W(self.g(self.V(torch.tensor(embedded_data)))))
+        return self.log_softmax(self.W(self.g(self.V(x))))
+
+def get_batches(data, batch_size):
+    count = 0
+    batches = []
+    count_down = len(data)
+    while count_down > batch_size:
+        batches.append(data[count:(count + batch_size)])
+        count += batch_size
+        count_down -= batch_size
+    if count_down > 1:
+        batches.append(data[count:])
+    
+    return batches
+
+def get_labels_and_data(batch):
+    labels = []
+    data = []
+    for datem, label in batch:
+        labels.append(label)
+        data.append(datem.numpy())
+    return data, labels
+
+
 
 def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_exs: List[SentimentExample], word_embeddings: WordEmbeddings) -> NeuralSentimentClassifier:
     """
@@ -83,7 +127,6 @@ def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_ex
     :param dev_exs: development set, in case you wish to evaluate your model during training
     :param word_embeddings: set of loaded word embeddings
     :return: A trained NeuralSentimentClassifier model
-
     """
     # Define hyper parmeters and model
     num_epochs = 1
@@ -95,26 +138,31 @@ def train_deep_averaging_network(args, train_exs: List[SentimentExample], dev_ex
     optimizer = optim.Adam(model.parameters(), lr=initial_learning_rate)
     loss = torch.nn.NLLLoss() # because of soft max
 
-    train_data =  load_data(train_exs, word_embeddings)
-    dev_data = load_data(dev_exs, word_embeddings)
-
+    # Preprocess data
+    print("Preprocessing the Training data")
+    train_data = []
+    for item in train_exs:
+        train_data.append((model.preprocess(item.words), item.label))
 
     for epoch in range(num_epochs):
+        print("entering epoch %i" % epoch)
         # set epoch level varibles
         total_loss = 0.0
         accuracys = []
 
-        for data, labels in train_data:
-            print(torch.tensor(data).shape) #32 x 52
+        #Batch the data
+        random.shuffle(train_data)
+        batches = get_batches(train_data, batch_size)
 
-            # print(len(labels))
-            # run through model
+        for batch in batches:
+            batch_data, batch_label = get_labels_and_data(batch)
+
             model.zero_grad()
-            y_pred = model.forward(data)
+            y_pred = model.predict_all(torch.tensor(batch_data), True)
             
             # calculate loss and accuracy
-            total_loss += loss(y_pred, labels)
-            accuracys.append(calculate_accuracy(y_pred, labels))
+            total_loss += loss(y_pred, batch_label)
+            accuracys.append(calculate_accuracy(y_pred, batch_label))
             
             # Computes the gradient and takes the optimizer step
             loss.backward()
